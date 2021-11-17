44d3567815e7d039d1c16720ba684e561ec11e39
Eduardo <eduardo.martinez@rseg.com>
2021-11-17 08:54:22 -0600

    Changes to environment and requirements and dvc for Heroku

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ from census_class import CensusData
 
 import git
 
+import os
+
 repo = git.Repo('.', search_parent_directories=True)
 repo_pth = repo.working_tree_dir
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 # Create the app object
